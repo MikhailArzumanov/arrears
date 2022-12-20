eb2c122b80cfc279817c850aa1800f0f10ed9458
Mikhail <mikhailArzumanov@yandex.ru>
2022-12-20 11:46:03 +0300

    Были добавлены миграции, были настроены параметры приложения и запуска

---
+++ Migrations/20221219114429_InitialMigration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace arrearsApi5_0.Migrations{
+    public partial class InitialMigration : Migration{
+        protected override void Up(MigrationBuilder migrationBuilder){
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder){
+
+        }
+    }
+}
---
+++ Migrations/20221219114953_ModelsAddition.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace arrearsApi5_0.Migrations{
+    public partial class ModelsAddition : Migration{
+        protected override void Up(MigrationBuilder migrationBuilder){
+            migrationBuilder.CreateTable(
+                name: "Disciplines",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    ShortName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Disciplines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    ShortName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Faculties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    ShortName = table.Column<string>(type: "text", nullable: true),
+                    FacultyId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Departments_Faculties_FacultyId",
+                        column: x => x.FacultyId,
+                        principalTable: "Faculties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    DepartmentId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Groups_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Magisters",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Surname = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PatronymicName = table.Column<string>(type: "text", nullable: true),
+                    DepartmentId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Magisters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Magisters_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DisciplineGroup",
+                columns: table => new{
+                    DisciplinesId = table.Column<int>(type: "integer", nullable: false),
+                    GroupsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_DisciplineGroup", x => new { x.DisciplinesId, x.GroupsId });
+                    table.ForeignKey(
+                        name: "FK_DisciplineGroup_Disciplines_DisciplinesId",
+                        column: x => x.DisciplinesId,
+                        principalTable: "Disciplines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DisciplineGroup_Groups_GroupsId",
+                        column: x => x.GroupsId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Surname = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PatronymicName = table.Column<string>(type: "text", nullable: true),
+                    GroupId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DisciplineMagister",
+                columns: table => new{
+                    DisciplinesId = table.Column<int>(type: "integer", nullable: false),
+                    MagistersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_DisciplineMagister", x => new { x.DisciplinesId, x.MagistersId });
+                    table.ForeignKey(
+                        name: "FK_DisciplineMagister_Disciplines_DisciplinesId",
+                        column: x => x.DisciplinesId,
+                        principalTable: "Disciplines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DisciplineMagister_Magisters_MagistersId",
+                        column: x => x.MagistersId,
+                        principalTable: "Magisters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArrearSheets",
+                columns: table => new{
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DisciplineId = table.Column<int>(type: "integer", nullable: false),
+                    StudentId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>{
+                    table.PrimaryKey("PK_ArrearSheets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ArrearSheets_Disciplines_DisciplineId",
+                        column: x => x.DisciplineId,
+                        principalTable: "Disciplines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArrearSheets_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArrearSheets_DisciplineId",
+                table: "ArrearSheets",
+                column: "DisciplineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArrearSheets_StudentId",
+                table: "ArrearSheets",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_FacultyId",
+                table: "Departments",
+                column: "FacultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DisciplineGroup_GroupsId",
+                table: "DisciplineGroup",
+                column: "GroupsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DisciplineMagister_MagistersId",
+                table: "DisciplineMagister",
+                column: "MagistersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_DepartmentId",
+                table: "Groups",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Magisters_DepartmentId",
+                table: "Magisters",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GroupId",
+                table: "Students",
+                column: "GroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder){
+            migrationBuilder.DropTable(
+                name: "ArrearSheets");
+
+            migrationBuilder.DropTable(
+                name: "DisciplineGroup");
+
+            migrationBuilder.DropTable(
+                name: "DisciplineMagister");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Disciplines");
+
+            migrationBuilder.DropTable(
+                name: "Magisters");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+        }
+    }
+}
