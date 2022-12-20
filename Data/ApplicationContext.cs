7d760b3c60d9f7ad2669b3ac0e23ae602d5e497a
Mikhail <mikhailArzumanov@yandex.ru>
2022-12-20 11:40:44 +0300

    Контекст был добавлен

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using arrearsApi5_0.Models;
+
+namespace arrearsApi5_0.Data{
+    public class ApplicationContext : DbContext{
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {
+            //Database.EnsureCreated();
+        }
+        public DbSet<ArrearSheet> ArrearSheets { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Discipline> Disciplines { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Magister> Magisters { get; set; }
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
