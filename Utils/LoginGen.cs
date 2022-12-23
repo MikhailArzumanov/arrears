2967a0667f183abffb5ef16e2d5b06d7dce6e879
Mikhail <mikhailArzumanov@yandex.ru>
2022-12-23 10:34:05 +0300

    Был добавлен генератор логинов

---
+++ Utils/LoginGen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace arrearsApi5_0.Utils{
+    public class LoginGen{
+        public int i = 0;
+        public string currentLogin = "";
+        public string Next(){
+            currentLogin = "";
+            do{
+                var currentDigit = i % 52;
+                char currentLetter = Convert.ToChar(currentDigit > 25 ? 'A' + currentDigit - 26 : 'a' + currentDigit);
+                currentLogin += currentLetter;
+                i /= 52;
+            } while (i > 0);
+            return currentLogin;
+        }
+    }
+}
