94cec1b015cd5106bd8da7ee3c4f46805def26c9
Mikhail <mikhailArzumanov@yandex.ru>
2022-12-26 10:04:00 +0300

    Модель дисциплин была доработана, была сформирована миграция

---
+++ Migrations/20221225153220_DisciplineModelRedaction.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace arrearsApi5_0.Migrations{
+    public partial class DisciplineModelRedaction : Migration{
+        protected override void Up(MigrationBuilder migrationBuilder){
+            migrationBuilder.AddColumn<string>(
+                name: "PassType",
+                table: "Disciplines",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Semestr",
+                table: "Disciplines",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Disciplines",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder){
+            migrationBuilder.DropColumn(
+                name: "PassType",
+                table: "Disciplines");
+
+            migrationBuilder.DropColumn(
+                name: "Semestr",
+                table: "Disciplines");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Disciplines");
+        }
+    }
+}
--- Models/Discipline.cs
+++ Models/Discipline.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace arrearsApi5_0.Models{
     public class Discipline{
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id           { get; set; }
+        public string Name      { get; set; }
         public string ShortName { get; set; }
+        public string PassType  { get; set; }
+        public string Year      { get; set; }
+        public string Semestr   { get; set; }
         public ICollection<Group> Groups { get; set; } = new List<Group>();
         public ICollection<Magister> Magisters { get; set; } = new List<Magister>();
     }
