13775ce723e034dfb6bd8bc8c504459613b7ce67
Mikhail <mikhailArzumanov@yandex.ru>
2022-12-26 20:11:35 +0300

    Была доработана валидация авторизационных данных

--- Utils/AuthValidation.cs
+++ Utils/AuthValidation.cs
@@ -8,14 +8,61 @@ using arrearsApi5_0.Models;
 namespace arrearsApi5_0.Utils{
     public class AuthValidation {
         
+        public static bool isAuthValidMagister(ArrearSheet arrearSheet, AuthData authData, string authType) {
+            var login = authData.Login;
+            var password = PasswordHasher.Hash(authData.Password);
+            switch (authType) {
+                case "magister":
+                    if (arrearSheet.Magister.Login != login || arrearSheet.Magister.Password != password)
+                        return false;
+                    break;
+                case "department":
+                    if (arrearSheet.Magister.Department.Login != login || arrearSheet.Magister.Department.Password != password)
+                        return false;
+                    break;
+                case "faculty":
+                    if (arrearSheet.Magister.Department.Faculty.Login != login || arrearSheet.Magister.Department.Faculty.Password != password)
+                        return false;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool isAuthValid(ArrearSheet arrearSheet, AuthData authData, string authType) {
+            var login = authData.Login;
+            var password = PasswordHasher.Hash(authData.Password);
+            switch (authType) {
+                case "student":
+                    if (arrearSheet.Student.Login != login || arrearSheet.Student.Password != password)
+                        return false;
+                    break;
+                case "group":
+                    if (arrearSheet.Student.Group.Login != login || arrearSheet.Student.Group.Password != password)
+                        return false;
+                    break;
+                case "department":
+                    if (arrearSheet.Student.Group.Department.Login != login || arrearSheet.Student.Group.Department.Password != password)
+                        return false;
+                    break;
+                case "faculty":
+                    if (arrearSheet.Student.Group.Department.Faculty.Login != login || arrearSheet.Student.Group.Department.Faculty.Password != password)
+                        return false;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
         public static bool isAuthValid(Student student, AuthData authData, string authType, bool selfRedaction = false) {
             var login = authData.Login;
             var password = PasswordHasher.Hash(authData.Password);
             switch (authType) {
                 case "student":
-                    if (student.Login != login || student.Password != password)
+                    if (student.Login == login && student.Password == password)
                         return selfRedaction;
-                    break;
+                    else return false;
                 case "group":
                     if (student.Group.Login != login || student.Group.Password != password)
                         return false;
@@ -38,9 +85,9 @@ namespace arrearsApi5_0.Utils{
             var password = PasswordHasher.Hash(authData.Password);
             switch (authType) {
                 case "magister":
-                    if (magister.Login != login || magister.Password != password)
+                    if (magister.Login == login && magister.Password == password)
                         return selfRedaction;
-                    break;
+                    else return false;
                 case "department":
                     if (magister.Department.Login != login || magister.Department.Password != password)
                         return false;
@@ -59,9 +106,9 @@ namespace arrearsApi5_0.Utils{
             var password = PasswordHasher.Hash(authData.Password);
             switch (authType) {
                 case "group":
-                    if (group.Login != login || group.Password != password)
+                    if (group.Login == login && group.Password == password)
                         return selfRedaction;
-                    break;
+                    else return false;
                 case "department":
                     if (group.Department.Login != login || group.Department.Password != password)
                         return false;
@@ -81,9 +128,9 @@ namespace arrearsApi5_0.Utils{
             var password = PasswordHasher.Hash(authData.Password);
             switch (authType) {
                 case "department":
-                    if (department.Login != login || department.Password != password)
+                    if (department.Login == login && department.Password == password)
                         return selfRedaction;
-                    break;
+                    else return false;
                 case "faculty":
                     if (department.Faculty.Login != login || department.Faculty.Password != password)
                         return false;
