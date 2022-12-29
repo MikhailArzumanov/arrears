98cd16f8e6e428e10b4560e7dbae2ea691ea89db
Mikhail <mikhailArzumanov@yandex.ru>
2022-12-29 09:58:39 +0300

    Был добавлен сервис администратора

---
+++ static/-services/administrators.service.js
@@ -0,0 +1,19 @@
+import { http } from '../-http/http.js';
+import { AuthData } from '../-models/auth-data.model.js';
+import {AuthorizedService} from './-base-services/authorized.service.js';
+
+export class AdministratorService extends AuthorizedService{
+    static CONTROLLER_NAME = 'administrator';
+    static CONTROLLER_URL  = this.SERVICE_URL+this.CONTROLLER_NAME;
+
+    static async login(login, password){
+        let METHOD_NAME = 'token'
+        let url = `${this.CONTROLLER_URL}/${METHOD_NAME}`;
+        let headers = {};
+            headers['Content-Type'] = "application/json;charset=UTF-8";
+        let params = {};
+        let body = new AuthData(login, password);
+        
+        return await http.post(url,headers,params,body,false);
+    }
+}
